17f6297b0572c80197d500302d67a8418a1826f8
Shinji Kobayashi <kobashin.masters@gmail.com>
2020-07-11 13:02:18 +0900

    サンプルアプリの雛形実装

--- app/src/main/java/com/kobashin/example/app/MainActivity.kt
+++ app/src/main/java/com/kobashin/example/app/MainActivity.kt
@@ -2,11 +2,19 @@ package com.kobashin.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
